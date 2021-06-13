eccf7f67f5ba179138ef9147a5596c911fb9bbc5
KimKiYoung <rlarldud6482@naver.com>
2021-06-14 08:46:06 +0900

    Create : WebView

---
+++ WebView/WebView/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  WebView
+//
+//  Created by 김기영 on 2021/06/14.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var txtUrl: UITextField!
+    @IBOutlet var myWebView: WKWebView!
+    @IBOutlet var myActivityIndicator: UIActivityIndicatorView!
+    
+    func loadWebPage(_ url: String) {
+        let myUrl = URL(string: url)
+        let myRequest = URLRequest(url: myUrl!)
+        myWebView.load(myRequest)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        loadWebPage("https://www.naver.com")
+    }
+
+    @IBAction func btnGotoUrl(_ sender: UIButton) {
+    }
+    @IBAction func btnGoSite1(_ sender: UIButton) {
+    }
+    @IBAction func btnGoSite2(_ sender: UIButton) {
+    }
+    @IBAction func btnLoadHtmlString(_ sender: UIButton) {
+    }
+    @IBAction func btnLoadHtmlFile(_ sender: UIButton) {
+    }
+    @IBAction func btnStop(_ sender: UIBarButtonItem) {
+    }
+    @IBAction func btnReload(_ sender: UIBarButtonItem) {
+    }
+    @IBAction func btnGoBack(_ sender: UIBarButtonItem) {
+    }
+    @IBAction func btnGoForward(_ sender: UIBarButtonItem) {
+    }
+}
+
