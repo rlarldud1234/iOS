0621aebf7e7edd2395536c446459ac2f1b997f63
KimKiYoung <rlarldud6482@naver.com>
2021-09-30 20:23:58 +0900

    swiftUIStudy : Start
    스위프트 유아이 공부 시작하기

---
+++ SwiftUIStudy/SwiftUIStudy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIStudy
+//
+//  Created by 김기영 on 2021/09/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIStudy/SwiftUIStudy/SwiftUIStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStudyApp.swift
+//  SwiftUIStudy
+//
+//  Created by 김기영 on 2021/09/30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
